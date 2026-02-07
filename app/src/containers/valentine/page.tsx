9338a4114b007142876942929d6d49d0f5199bcf
serenochan <75026618+serenochan@users.noreply.github.com>
2026-02-07 21:05:33 +0800

    Update page.tsx

--- app/src/containers/valentine/page.tsx
+++ app/src/containers/valentine/page.tsx
@@ -7,21 +7,19 @@ const noTextsArr = [
   "No",
   "Are you sure?",
   "Really?",
-  "Are you positive",
+  "Are you positive?",
   "Just think about it",
-  "if you say no, I'll be very sad",
+  "If you say no, I'll be very sad",
   "I'll be very sad",
   "I'll be very very very sad",
   "Ok fine, I'll stop asking...",
   "Just kidding, PLEASE SAY YES",
   "You're breaking my heart ;(",
-  "No",
 ];
 
 enum Stages {
   stageNo = "stage-no",
   stageYes = "stage-yes",
-  unhappy = "unhappy",
 }
 
 export default function Valentine() {
@@ -29,17 +27,8 @@ export default function Valentine() {
   const [counter, setCounter] = useState(0);
 
   const changeNoText = () => {
-    setCounter((prevState) => {
-      if (prevState < noTextsArr.length - 1) {
-        return prevState + 1;
-      } else {
-        setStage(Stages.unhappy);
-        return 0;
-      }
-    });
+    setCounter((prev) => (prev + 1) % noTextsArr.length);
   };
-
-  console.log(counter);
 
   return (
     <section className="valentine-box select-none">
@@ -54,17 +43,20 @@ export default function Valentine() {
               height={250}
             />
           </div>
+
           <div className="text-2xl mt-2 text-gray-900 text-center">
             Will you be my Valentine?
           </div>
+
           <div className="flex items-center justify-center mt-3 text-center">
             <button
-              className="bg-green-500 text-white rounded me-2 py-1 px-2"
-              style={{ fontSize: counter ? `${(counter + 1) * 15}px` : "18px" }}
-              onClick={() => setStage(Stages.stageYes)}
-            >
-              Yes
-            </button>
+  className="bg-green-500 text-white rounded me-2 py-1 px-2"
+  style={{ fontSize: counter ? `${(counter + 1) * 15}px` : "18px" }}
+  onClick={() => setStage(Stages.stageYes)}
+>
+  Yes, crushie
+</button>
+
             <button
               className="bg-red-500 text-white rounded ms-2 py-1 px-2 text-[18px]"
               onClick={changeNoText}
@@ -74,6 +66,7 @@ export default function Valentine() {
           </div>
         </>
       )}
+
       {stage === Stages.stageYes && (
         <>
           <Image
@@ -83,20 +76,7 @@ export default function Valentine() {
             height={250}
           />
           <div className="text-center text-2xl mt-2 text-gray-900">
-            {"Yay! Now I'm happy!"}
-          </div>
-        </>
-      )}
-      {stage === Stages.unhappy && (
-        <>
-          <Image
-            src="/valentine/unhappy-bear.gif"
-            alt="valentine kiss | valentine"
-            width={250}
-            height={250}
-          />
-          <div className="text-center text-2xl mt-2 text-gray-900">
-            {"OK! Bye..."}
+            Yay! Now I&apos;m happy!
           </div>
         </>
       )}
