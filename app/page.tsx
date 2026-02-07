a5d4bf51b6881dcc9a7684f2b3d29480781ef2e4
serenochan <75026618+serenochan@users.noreply.github.com>
2026-02-07 21:27:36 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import Valentine from "@src/containers/valentine/page";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-white">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Valentine />
-      </main>
+    <div className="min-h-screen w-full bg-transparent flex items-center justify-center">
+      <Valentine />
     </div>
   );
 }
